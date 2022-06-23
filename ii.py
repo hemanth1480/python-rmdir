be9e0f3f4e439eb00c91bb866faf0cc356abb4bf
hemanth1480 <71897198+hemanth1480@users.noreply.github.com>
2022-06-23 22:08:16 +0530

    Update ii.py

--- ii.py
+++ ii.py
@@ -1,9 +1,6 @@
-import os
-from pydoc import doc
-from docx2pdf import convert
-
-print(os.path.dirname(os.path.abspath(__file__)))
-
-docs = ['DP4 REPORt-corr.docx','final report dp4..11.docx']
-for dd in docs:
-    convert(os.path.dirname(os.path.abspath(__file__)) + "/files/" + dd , dd.split(".")[0] + ".pdf")
+import shutil 
+import os 
+# location 
+location = "converter"
+# removing directory 
+shutil.rmtree(location) 
